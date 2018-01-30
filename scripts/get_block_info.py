35f777c6e62b5a768f9d8ef286a4b1e4030e4fd3
Tao Yiran <taoyiran@localhost>
2018-01-30 11:38:53 +0800

    finished the block information file

--- scripts/get_block_info.py
+++ scripts/get_block_info.py
@@ -1,7 +1,7 @@
 import sys
 import json
 
-DINANPING_BLOCK_KEYS = {
+DIANPING_BLOCK_KEYS = {
     'total_price',
     'total_comments',
     'total_shop_num'
@@ -11,18 +11,20 @@ LIANJIA_BLOCK_KEYS = {
     'total_deal_price',
     'total_rent_price',
     'total_onsale_price',
+    'total_lianjia_shop_num'
 }
 
 
 class GetBlockInfo:
     """get block information"""
 
-    def __init__(self, data_file):
-        self.data_file = data_file
+    def __init__(self, dianping_file, lianjia_file):
+        self.dianping_file = dianping_file 
+        self.lianjia_file = lianjia_file
         self.block_dict = {}
 
     def _read_dianping_data(self):
-        with open(self.data_file) as fp:
+        with open(self.dianping_file) as fp:
             for line in fp:
                 line = line.strip()
                 fields = line.split('\t')
@@ -51,7 +53,7 @@ class GetBlockInfo:
                 self.block_dict[block_index]['total_shop_num'] += 1
 
     def _read_lianjia_data(self):
-        with open(self.data_file) as fp:
+        with open(self.lianjia_file) as fp:
             for line in fp:
                 line = line.strip()
                 fields = line.split('\t')
@@ -68,32 +70,47 @@ class GetBlockInfo:
                 # deal price
                 deal_p = fields[8]
                 if deal_p:
-                    self.block_dict[block_index]['total_deal_price'] += int(deal_p)
+                    self.block_dict[block_index]['total_deal_price'] += float(deal_p)
 
                 # onsale price
-                # TODO: calculate every keys...
+                sale_p = fields[5]
+                if sale_p:
+                    self.block_dict[block_index]['total_onsale_price'] += float(sale_p)
 
-    def format_dianping_block_info(self):
+                # rent price
+                rent_p = fields[7]
+                if rent_p:
+                    self.block_dict[block_index]['total_rent_price'] += float(rent_p)
+
+                # lianjia shop number
+                self.block_dict[block_index]['total_lianjia_shop_num'] += 1
+
+
+    def format_block_info(self):
         self._read_dianping_data()
-        print('block_id\ttotal_price\ttotal_comments\ttotal_shop_number')
+        self._read_lianjia_data()
+        print('block_id\ttotal_price\ttotal_comments\ttotal_shop_number\ttotal_deal_price\ttotal_onsale_price\ttotal_rent_price')
         for index, body in self.block_dict.items():
             block_info_list = []
             block_info_list.append(index)
             for k in DIANPING_BLOCK_KEYS:
-                block_info_list.append(str(body[k]))
+                if k in body:
+                    block_info_list.append(str(body[k]))
+                else:
+                    block_info_list.append('')
+            for k in LIANJIA_BLOCK_KEYS:
+                if k in body:
+                    block_info_list.append(str(body[k]))
+                else:
+                    block_info_list.append('')
             print('\t'.join(block_info_list))
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print('Usage: python3 {} data_file [dianping | lianjia]'.format(sys.argv[0]))
+        print('Usage: python3 {} dianping_file, lianjia_file'.format(sys.argv[0]))
     else:
-        bi = GetBlockInfo(sys.argv[1])
-        if sys.argv[2] == 'dianping':
-            bi.format_dianping_block_info()
-        elif sys.argv[2] == 'lianjia':
-            bi.format_lianjia_block_info()
-        else:
-            print('Usage: python3 {} data_file [dianping | lianjia]'.format(sys.argv[0]))
+        bi = GetBlockInfo(sys.argv[1], sys.argv[2])
+        bi.format_block_info()
 
 
